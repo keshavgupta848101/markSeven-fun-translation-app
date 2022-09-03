e7fe39e73b36640b45c4c60de2cafa0e89b998d8
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2022-09-03 12:28:57 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# markSeven: Yoda Translation”
+
+# About Application
+
+1. Used Use Fun translation API
+2. The app take in input and translate the input into Minion's language.
+3. Display the output
+
+# Main Screen
+
+![image](https://res.cloudinary.com/debo7pflq/image/upload/v1662188276/github/Screenshot_1494_h90dnk.png)
+
+# Live Link
+
+[Live Link]()
---
+++ app.js
@@ -0,0 +1,26 @@
+const textarea = document.getElementById('textarea');
+const buton = document.getElementById('buton');
+const outputArea = document.getElementById('outputArea');
+
+
+buton.addEventListener('click', print);
+
+
+function handelerr(e) {
+    console.log(e);
+    alert("Something wrong with the server, Try again latter");
+}
+
+function print() {
+
+    const value = textarea.value;
+    fetch(`https://api.funtranslations.com/translate/yoda.json?text=${value}`)
+        .then((APIDATA) => {
+            return APIDATA.json();
+        }).then((REALDATA) => {
+            outputArea.innerText = REALDATA.contents.translated;
+        }).catch((error) => {
+            handelerr(error)
+        });
+}
+
